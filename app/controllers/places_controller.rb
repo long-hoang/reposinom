68b827103481aee8dcbf0dbcc6ecf7a9dc17490c
long-hoang <thoanglong@gmail.com>
2019-06-26 06:19:34 +0000

    added form and submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,18 @@ class PlacesController < ApplicationController
 
 
   def new
-    @place = Place.new
+    @place = Place.new # creates the FORM for place
+  end
+
+  def create
+    Place.create(place_params) # sends values to the database
+    redirect_to root_path # sends user to the root
+  end
+
+  private 
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address) # pulls values from place FORM
   end
 
 end
