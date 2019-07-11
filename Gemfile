5d21b647954468f542c4f170f3c0a39a2a91bf7b
long-hoang <thoanglong@gmail.com>
2019-07-11 17:07:56 +0000

    added geocoding feature

--- Gemfile
+++ Gemfile
@@ -21,8 +21,9 @@ gem 'uglifier', '>= 1.3.0'
 
 gem 'jquery-rails'
 
+gem "geocoder"
 
-
+gem "figaro"
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   validates :address, :description, presence: true # prevents new place from being saved without a valid entry
   validates :name, presence: true, length: { minimum: 3 }
 
