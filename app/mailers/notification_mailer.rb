503f57824754232e3ac67b08fc589e1484b21221
long-hoang <thoanglong@gmail.com>
2019-07-17 07:37:42 +0000

    mailer function works

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "thoanglong@gmail.com",
+    mail(to: "lthoang0@gmail.com",
           subject: "A comment has been added to your place.")
   end
 
